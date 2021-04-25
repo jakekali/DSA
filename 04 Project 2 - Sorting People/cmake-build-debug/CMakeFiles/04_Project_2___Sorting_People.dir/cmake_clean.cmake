fdf821a93a3d7fb6c59e85a490cd43d3b0083f31
Jacob Khalili <jakekali@users.noreply.github.com>
2021-04-25 18:16:25 -0400

    dd

---
+++ 04 Project 2 - Sorting People/cmake-build-debug/CMakeFiles/04_Project_2___Sorting_People.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "04_Project_2___Sorting_People.exe"
+  "04_Project_2___Sorting_People.pdb"
+  "CMakeFiles/04_Project_2___Sorting_People.dir/main.cpp.o"
+  "lib04_Project_2___Sorting_People.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/04_Project_2___Sorting_People.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
